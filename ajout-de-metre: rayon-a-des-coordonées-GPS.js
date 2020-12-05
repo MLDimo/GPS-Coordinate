26b7aeb1c65ee7501797d332a73d5d241b7f2910
martin laroche <73097502+MLDimo@users.noreply.github.com>
2020-12-05 11:06:58 +0100

    Add files via upload

---
+++ ajout-de-metre: rayon-a-des-coordonées-GPS.js
@@ -0,0 +1,36 @@
+
+
+
+var earth = 6378.137;
+var pi = Math.PI;
+var cos = Math.cos;
+var m = (1 / ((2 * pi / 360) * earth)) / 1000;
+
+
+
+/* Add meter  nb_m 
+
+ var nb_m = your extra meter*/
+
+
+//latitude
+
+var newLatitude = latitude + (nb_m * m)     /* latitude = original latitude / nb_m = number of meter */
+
+
+// longitude
+
+var newLongitude = longitude + (nb_m * m) / cos(latitude * (pi / 180)); /* longitude = original longitude/latitude = original latitude / nb_m = number of meter */
+
+
+
+
+/* Add a radius r 
+
+ var r = your radius*/
+
+var minLat = latitude - (r * m)
+var maxLat = latitude + (r * m)
+
+var minLong = longitude - (r * m) / cos(latitude * (pi / 180))
+var maxLong = longitude + (r * m) / cos(latitude * (pi / 180))
